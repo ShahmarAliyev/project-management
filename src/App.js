715de008d408a5004dc23c28a368ad1dfc28a057
ShahmarALiyev <saliyev2019@gmail.com>
2022-02-21 10:07:57 -0600

    Created pages and added react router dom 6.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Login from "./pages/Login/Login";
+import Create from "./pages/Create/Create";
+import Signup from "./pages/Signup/Signup";
+import Project from "./pages/Project/Project";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/project" element={<Project />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
