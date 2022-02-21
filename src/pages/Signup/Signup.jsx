333bb210c956deab518360923ddb288f47f8cf1a
ShahmarALiyev <saliyev2019@gmail.com>
2022-02-21 14:12:39 -0600

    created signup page froms

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Signup.css";
+
 function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [displayName, setDisplayName] = useState();
+  const [thumbnail, setThumbnail] = useState();
+
+  return (
+    <form className="auth-form">
+      <h2>Sign up</h2>
+      <label>
+        <span>email:</span>
+        <input
+          required
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          required
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <label>
+        <span>displayName:</span>
+        <input
+          required
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <label>
+        <span>profile thumbnail:</span>
+        <input required type="file" value={thumbnail} />
+      </label>
+
+      <button className="btn"> Signup</button>
+    </form>
+  );
 }
 
 export default Signup;
