7e39fae17c13effbc713871f6de53ff784ff0110
ShahmarALiyev <saliyev2019@gmail.com>
2022-02-21 21:58:23 -0600

    added user avatar components

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,16 +1,22 @@
-import React from "react";
-
 import "./Sidebar.css";
-import DashboardIcon from "../../assets/dashboard_icon.svg";
-import AddIcon from "../../assets/add_icon.svg";
+
+import { useAuthContext } from "../../hooks/useAuthContext";
+
 import { NavLink } from "react-router-dom";
 
+import Avatar from "../Avatar/Avatar";
+
+import DashboardIcon from "../../assets/dashboard_icon.svg";
+import AddIcon from "../../assets/add_icon.svg";
+
 function Sidebar() {
+  const { user } = useAuthContext();
   return (
     <div className="sidebar">
       <div className="sidebar-content">
         <div className="user">
-          <p>Hey User!</p>
+          <Avatar src={user.photoURL} />
+          <p>Hey {user.displayName}</p>
         </div>
         <nav className="links">
           <ul>
