b6a5986393a9150a2679815f9cdd5e97cc0d72c0
ShahmarALiyev <saliyev2019@gmail.com>
2022-02-21 21:43:57 -0600

    Redirects, Route Guarding, and conditinal navbar links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import { useAuthContext } from "./hooks/useAuthContext";
 
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Login from "./pages/Login/Login";
@@ -9,22 +11,40 @@ import Project from "./pages/Project/Project";
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
 function App() {
+  const { user, authIsReady } = useAuthContext();
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Sidebar />
-
-        <div className="container">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/create" element={<Create />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/project" element={<Project />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <Sidebar />
+          <div className="container">
+            <Navbar />
+            <Routes>
+              <Route
+                path="/"
+                element={user ? <Dashboard /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/login"
+                element={!user ? <Login /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/create"
+                element={user ? <Create /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/signup"
+                element={!user ? <Signup /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/projects/:id"
+                element={user ? <Project /> : <Navigate to="/projects/:id" />}
+              />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { useLogout } from "../../hooks/useLogout";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 import "./Navbar.css";
 import Temple from "../../assets/temple.svg";
 import { Link } from "react-router-dom";
 function Navbar() {
+  const { user } = useAuthContext();
   const { logout, isPending } = useLogout();
 
   return (
@@ -14,22 +16,27 @@ function Navbar() {
           <img src={Temple} alt="logo" />
           <span>The Rocket</span>
         </li>
+        {!user && (
+          <>
+            <li>
+              <Link to="login"> Login</Link>
+            </li>
+            <li>
+              <Link to="signup">Signup </Link>
+            </li>
+          </>
+        )}
 
-        <li>
-          <Link to="login"> Login</Link>
-        </li>
-        <li>
-          <Link to="signup">Signup </Link>
-        </li>
-
-        <li>
-          {!isPending && (
-            <button className="btn" onClick={logout}>
-              Logout
-            </button>
-          )}
-          {isPending && <button className="btn">Logging out...</button>}
-        </li>
+        {user && (
+          <li>
+            {!isPending && (
+              <button className="btn" onClick={logout}>
+                Logout
+              </button>
+            )}
+            {isPending && <button className="btn">Logging out...</button>}
+          </li>
+        )}
       </ul>
     </div>
   );
