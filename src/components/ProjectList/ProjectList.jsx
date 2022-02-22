45d96ebfbc1fb72b08edb668a27659658be58435
ShahmarALiyev <saliyev2019@gmail.com>
2022-02-22 12:05:55 -0600

    created project list grid

--- src/components/ProjectList/ProjectList.jsx
+++ src/components/ProjectList/ProjectList.jsx
@@ -1,10 +1,28 @@
+import { Link } from "react-router-dom";
+import Avatar from "../Avatar/Avatar";
 import "./ProjectList.css";
 function ProjectList({ projects }) {
   return (
-    <div>
+    <div className="project-list">
       {projects.length === 0 && <p>No Projects Yet</p>}
       {projects.map((project) => {
-        return <div key={project.id}>{project.name}</div>;
+        return (
+          <Link to={`/projects/${project.id}`} key={project.id}>
+            <h4>{project.name}</h4>
+            <p> Due by {project.dueDate.toDate().toDateString()}</p>
+            <div className="assigned-to">
+              <ul>
+                {project.assignedUsersList.map((u) => {
+                  return (
+                    <li key={u.photoURL}>
+                      <Avatar src={u.photoURL} />
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </Link>
+        );
       })}
     </div>
   );
