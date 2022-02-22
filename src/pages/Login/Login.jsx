9ee034faa940f4307e3a0c743b025b38254347b3
ShahmarALiyev <saliyev2019@gmail.com>
2022-02-21 20:38:14 -0600

    logging in users

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import { useState } from "react";
+import { useLogin } from "../../hooks/useLogin";
 import "./Login.css";
+
 function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { login, isPending, error } = useLogin();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    login(email, password);
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="auth-form">
+      <h2>Login</h2>
+      <label>
+        <span>email:</span>
+        <input
+          required
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          required
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+
+      {!isPending && <button className="btn">Login</button>}
+      {isPending && (
+        <button className="btn" disabled>
+          Logging in...
+        </button>
+      )}
+
+      {error && <div className="error">{error}</div>}
+    </form>
+  );
 }
 
 export default Login;
