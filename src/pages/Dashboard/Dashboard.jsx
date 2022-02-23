c11e8918bdb7c9a0ca1c9d4cecbf8ff5bafa3994
ShahmarALiyev <saliyev2019@gmail.com>
2022-02-23 15:39:08 -0600

    Filterin out projects based on categories

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,15 +1,39 @@
 import { useState } from "react";
 import "./Dashboard.css";
+import { useAuthContext } from "../../hooks/useAuthContext";
 import { useCollection } from "../../hooks/useCollection";
 import ProjectList from "../../components/ProjectList/ProjectList";
 import ProjectFilter from "./ProjectFilter";
 function Dashboard() {
+  const { user } = useAuthContext();
   const [currentFilter, setCurrentFilter] = useState("all");
   const { documents, error } = useCollection("projects");
 
   const changeFilter = (newFilter) => {
     setCurrentFilter(newFilter);
   };
+
+  const filteredProjects = documents
+    ? documents.filter((document) => {
+        switch (currentFilter) {
+          case "all":
+            return true;
+          case "mine":
+            let assignedToMe = false;
+            document.assignedUsersList.forEach((u) => {
+              if (user.uid === u.id) assignedToMe = true;
+            });
+            return assignedToMe;
+          case "development":
+          case "design":
+          case "sales":
+          case "marketing":
+            return document.category === currentFilter;
+          default:
+            return true;
+        }
+      })
+    : null;
 
   return (
     <div>
@@ -21,7 +45,7 @@ function Dashboard() {
           changeFilter={changeFilter}
         />
       )}
-      {documents && <ProjectList projects={documents} />}
+      {filteredProjects && <ProjectList projects={filteredProjects} />}
     </div>
   );
 }
